cbba60d554f74b2c1b3ee1cdaa10cc8b511aa5e5
Arsenii <at437947@gmail.com>
2023-10-14 13:29:26 -0700

    Added a page
    Added a page with a dynamic address
    Added a new folder " show "

---
+++ senku-app/app/client/[clientid]/page.js
@@ -0,0 +1,11 @@
+export default function ClientId({params}) {
+    return (
+        <div>
+            <h1>ClientId page</h1>
+            <p>Текущий url адрес : "{params.clientid}"</p>  
+        </div>
+    )
+  }
+
+//   Отображается при любом url кроме show
+  
